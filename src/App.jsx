f9157f89cf5870665d117940b6f091a60974577e
Vinicius Sulpicio <145928303+Viniciusulpicio@users.noreply.github.com>
2024-04-29 14:42:55 +0000

    upload gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./Components/Header/Header.jsx";
 import Home from "./Components/Home/Home.jsx";
 import ConteudoUniverso from "./Components/ConteudoUniverso/ConteudoUniverso.jsx";
 import Vantagens from "./Components/Vantagens/Vantagens.jsx";
+import Conteudo from "./Components/Conteudo/Conteudo.jsx";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route Component={Header}/>
         <Route Component={Home} path="/Crianca-Inteligente" exact />
         <Route Component={ConteudoUniverso}/>
+        <Route Component={Conteudo}/>
         <Route Component={Vantagens}/>
         
       </Routes>
