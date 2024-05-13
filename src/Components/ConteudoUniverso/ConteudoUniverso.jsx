de461e37c5ba9f51f4ad5ce528f88b1446a82593
Viniciusulpicio <viniciusulpicio@gmail.com>
2024-05-13 13:56:09 -0300

    upload novas adições

--- src/Components/ConteudoUniverso/ConteudoUniverso.jsx
+++ src/Components/ConteudoUniverso/ConteudoUniverso.jsx
@@ -1,9 +1,13 @@
   import React from "react";
   import "../../Style.css";
-
-
+  import { Swiper, SwiperSlide } from 'swiper/react';
 
   const ConteudoUniverso = () => {
+
+    const data = [
+      {id: "1", image: "../../img/44a0e3f674d2d9f928ed00d18d5e8666.png"}, 
+      {id: "2", image: "../../img/35f6e088ec0e7b8099f2075e2cd1a22b-1.png"}
+    ]
     return (
       <>
         <div className="conteudoUniversoFundoAzul">
@@ -14,7 +18,23 @@
           </div>
           <div className="fundoConteudoUniversoCarrosel">
             <h2>Vídeos, músicas, brincadeiras E MUITO CONTEÚDO INTERATIVO</h2>
-            <div className="carrosselItens"></div>
+            <div className="carrosselItens">
+              <Swiper
+              slidesPerView={9}
+              pagination={{clickable: true}}
+              navigation
+              >
+                {data.map((item) => (
+                   <SwiperSlide key={item.id}>
+                    <img
+                    src={item.image}
+                    alt="Slider"
+                    className="slideItem"
+                    />
+                   </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
         </div>
       </>
--- src/Components/Vantagens/Vantagens.jsx
+++ src/Components/Vantagens/Vantagens.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import Globo from "../../img/planeta2.png"
 import Lua from "../../img/lua.png"
 
+import  Vaca from "../../img/imagem-8.png"
+import Mochila from "../../img/imagem-7.png"
+import Lapis from "../../img/imagem-9.png"
+import Estrela from "../../img/imagem-6.png"
+import Coracao from "../../img/imagem-11.png"
+import Pc from "../../img/imagem-10.png"
+
 const Vantagens = () => {
   return (
     <>
@@ -10,7 +17,46 @@ const Vantagens = () => {
     </div>
 
     <div className="container-vantagens">
-      <div className="globo"></div>
+      <div className="tituloVantagens">
+        <h1>Só <span className='laranja'>Crianças Inteligentes</span> <br /> equilibra de verdade o <br /> brincar e o aprender!</h1>
+        <p className='subtituloVantagens'>Conteúdo que acelera o aprendizado de forma lúdica e divertida, <br /> respeitando o ritmo de cada criança</p>
+
+        <div className="conteudoVantagens">
+        <div className="primeira">
+          <div className='div-flex'>
+            <img src={Vaca} alt="vaca" />
+            <p>Desenvolvido por especialistas em metodologia de ensino e musicalização</p>
+          </div>
+
+          <div className='div-flex'>
+            <img src={Mochila} alt="mochila" />
+            <p>Plataforma 100% segura e sem propagandas</p>
+          </div>
+
+          <div className='div-flex'>
+            <img src={Lapis} alt="lapis" />
+            <p>Baseado na BNCC (Base Nacional Comum Curricular) e no PNA (Plano Nacional de Alfabetização)</p>
+          </div>
+
+          </div>
+          <div className="secundaria">
+          <div className='div-flex'>
+            <img src={Estrela} alt="estrela" />
+            <p>Curadoria que garante a qualidade pedagógica de todo o conteúdo</p>
+          </div>
+
+          <div className='div-flex'>
+            <img src={Coracao} alt="coracao" />
+            <p>Acompanha seu filho desde o INFANTIL e até os primeiros anos do FUNDAMENTAL</p>
+          </div>
+
+          <div className='div-flex'>
+            <img src={Pc} alt="Computador" />
+            <p>Conteúdos novos toda semana</p>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>  
 
     <div className="lua-tela">
